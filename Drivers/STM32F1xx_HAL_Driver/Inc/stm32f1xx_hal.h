62fa6b6512ed9620bd32e55bffc64490f7cdad91
wiktor <comradewiktor@gmail.com>
2021-01-20 08:57:38 +0100

    Working copy

--- Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+++ Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
@@ -27,7 +27,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f1xx_hal_conf.h"
+#include <stm32f1xx_hal_conf.h>
 
 /** @addtogroup STM32F1xx_HAL_Driver
   * @{
